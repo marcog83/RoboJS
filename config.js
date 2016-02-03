aa4fa4590be19e8d0bcec9617ba99208a4a238b4
marcog83 <marcogobbi0@gmail.com>
2016-02-03 17:57:46 +0100

    gh-pages

--- config.js
+++ config.js
@@ -1,58 +1,11 @@
-System.config({
-  defaultJSExtensions: true,
-  transpiler: "traceur",
-  babelOptions: {
-    "optional": [
-      "runtime",
-      "optimisation.modules.system"
-    ]
-  },
-  paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
-  },
+/**
+ * Created by marco.gobbi on 10/11/2014.
+ */
+requirejs.config({
+    paths: {
 
-  map: {
-    "babel": "npm:babel-core@5.8.34",
-    "babel-runtime": "npm:babel-runtime@5.8.34",
-    "core-js": "npm:core-js@0.9.18",
-    "latest": "npm:babel-core@5.8.34",
-    "latest-runtime": "npm:babel-runtime@5.8.34",
-    "ramda": "npm:ramda@0.18.0",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
-    },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
-    },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:assert@1.3.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:babel-runtime@5.8.34": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:inherits@2.0.1": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:process@0.11.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
-    },
-    "npm:ramda@0.18.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:util@0.10.3": {
-      "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+        robojs: "../../dist/robojs.es6"
+
     }
-  }
 });
+require(["./client/Application"], function () {});
--- src/org/core/net/AMDScriptLoader.js
+++
@@ -1,4 +0,0 @@
-
-export default  Object.freeze({
-    load: id=> new Promise((resolve, reject)=> window.require([id],resolve,reject))
-});
