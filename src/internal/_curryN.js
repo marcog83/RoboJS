888d4bb8b0ac74ae5333f823f533f8db3fc2016e
marcog83 <marcogobbi0@gmail.com>
2018-07-05 16:21:06 +0200

    oop js

--- src/internal/_curryN.js
+++ src/internal/_curryN.js
@@ -2,20 +2,27 @@
  * Created by mgobbi on 20/04/2017.
  */
 import _arity from "./_arity";
+
 function _curryN(length, received, fn) {
-    return function() {
+    function getResult(combinedIdx, _argsIdx) {
+        let result;
+        if (combinedIdx < received.length) {
+            result = received[combinedIdx];
+        } else {
+            result = arguments[_argsIdx];
+            _argsIdx += 1;
+        }
+        return {result, _argsIdx};
+    }
+
+    return function () {
         const combined = [];
         let argsIdx = 0;
         let left = length;
         let combinedIdx = 0;
         while (combinedIdx < received.length || argsIdx < arguments.length) {
-            let result;
-            if (combinedIdx < received.length  ) {
-                result = received[combinedIdx];
-            } else {
-                result = arguments[argsIdx];
-                argsIdx += 1;
-            }
+            const {result, _argsIdx} = getResult(combinedIdx, argsIdx);
+            argsIdx = _argsIdx;
             combined[combinedIdx] = result;
             left -= 1;
             combinedIdx += 1;
@@ -24,4 +31,5 @@ function _curryN(length, received, fn) {
             : _arity(left, _curryN(length, combined, fn));
     };
 }
+
 export default _curryN;
